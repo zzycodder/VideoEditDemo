37a4657bc5185f49157a11df541eb7cf3fd4371b
ZHANGZHONGYANG <zhangzhongyang92@126.com>
2019-01-05 22:37:58 +0800

    Initial Commit

---
+++ VideoEditDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoEditDemo
+//
+//  Created by ZHANGZHONGYANG on 2019/1/5.
+//  Copyright © 2019年 ZHANGZHONGYANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoEditDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoEditDemo
+//
+//  Created by ZHANGZHONGYANG on 2019/1/5.
+//  Copyright © 2019年 ZHANGZHONGYANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
